2a5b56e6d23d56e77696176cc3d35e87284a9dfb
dfenjves <fenjves.daniel@gmail.com>
2016-03-14 12:21:33 -0400

    added solution

--- js/encoder.js
+++ js/encoder.js
@@ -1,6 +1,7 @@
 var encode = function(userString){
   //1. Add code to turn string to lowercase
-  
+    userString = userString.toLowerCase().replace(/e/g,"3").replace(/o/g,"0").replace(/i/g,"1").replace(/a/g,"4").replace(/ /g,"$")
+
   // 2. Add code to change each instance of a, e, i, o to the appropriate number based on l33n rules
 
   // 3. Add code to replace spaces with dollar signs.
