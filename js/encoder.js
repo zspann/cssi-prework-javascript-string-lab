a6b5b0e2abacc9bc2a1b094b9aa03cdb4fc070a1
Zack Spann <zspann@zspann-macbookpro4.roam.corp.google.com>
2017-03-09 11:52:28 -0800

    added some code?

--- js/encoder.js
+++ js/encoder.js
@@ -1,10 +1,13 @@
 var encode = function(userString){
   //1. Add code to turn string to lowercase
-  
+  userString.toLowerCase()
   // 2. Add code to change each instance of a, e, i, o to the appropriate number based on l33n rules
-
+  userString.replace(/a/g, 4)
+  userString.replace(/e/g, 3)
+  userString.replace(/i/g, 1)
+  userString.replace(/o/g, 0)
   // 3. Add code to replace spaces with dollar signs.
-
+  userString.replace(/ /g, $)
   //This line assigns the variable encode to the value userString
   return userString
 }
